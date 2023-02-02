68d0133d74b09833464b40c7d0551231a9123cc9
ShalomDauphin <shalomdauphin8@gmail.com>
2023-02-02 08:49:22 +0200

    ft: User details updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,6 +55,27 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+//@desc     gUpdate User Details
+//@routes   PUT/ api/v1/auth/updatedetails
+// access   private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+	const fieldsToUpdate = {
+		name: req.body.name,
+		email: req.body.email,
+	};
+
+	const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: user,
+	});
+});
+
+
 // get a token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // create token
